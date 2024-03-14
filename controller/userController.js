523ebdaea10dcb59c185314853ac8cc9eb3715e6
aliRagab2002 <tltaath30@gmail.com>
2024-03-14 05:55:01 +0200

    update

--- controller/userController.js
+++ controller/userController.js
@@ -75,7 +75,7 @@ const signup = async (req, res) => {
 };
 
 const sendVerificationEmail = ({_id,email},res)=>{
-    const currendUri = "http://localhost:5000/";
+    const currendUri = "https://aliragab752002-32d59c101d22.herokuapp.com/";
     const uniqueString = uuidv4() + _id;
     
     const mailOptions = {
